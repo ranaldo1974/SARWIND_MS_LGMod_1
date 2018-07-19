b22f131a6cb2366868b31fdd2624c84fd0caf658
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-19 15:29:50 +0200

    def

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-unzip /InData.zip 
+unzip /InData.zip
+if [ -f /tmp/.X1-lock ]; then
+    rm -f /tmp/.X1-lock
+fi
+if [ -d /tmp/.X11-unix ]; then
+    rm -rf /tmp/.X11-unix
+fi
+vnc4server
 ./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
+zip /OutData.zip ./OutData
