bb5284f776356cba8b47b55e3caca74351f529b3
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-23 15:48:51 +0200

    definitivo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "--------------------------------------------------------------"
-echo "CONTENT OF THE FOLDER"
+echo "CONTENT OF THE CURRENT FOLDER"
 ls -a .
 echo "--------------------------------------------------------------"
 unzip ./InData.zip 
@@ -10,9 +10,6 @@ if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
 vnc4server
-echo "--------------------------------------------------------------"
 export DISPLAY=$HOSTNAME:1
-echo $DISPLAY
-echo "--------------------------------------------------------------"
 ./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
 zip -r OutData.zip OutData
