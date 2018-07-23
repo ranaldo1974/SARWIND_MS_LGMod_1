52d1ea861f69720d724d38972bccd28ff0c5db9f
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-23 12:56:27 +0200

    impostati tutti i riferimenti alla cartella corrente

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
 vnc4server
-/run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 /InData/SARWIND_IO_Info.txt /InData/SARWIND_Settings.txt
-zip /OutData.zip /OutData
+./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
+zip /OutData.zip ./OutData
