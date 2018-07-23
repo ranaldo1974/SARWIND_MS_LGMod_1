b29e065d540f063ece2c56ebdca5a5f14cf2aa31
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-23 14:36:48 +0200

    modifiche

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-echo "-------------------------------"
+echo "--------------------------------------------------------------"
 echo "CONTENT OF THE FOLDER"
 ls -a .
-echo "-------------------------------"
+echo "--------------------------------------------------------------"
 echo "CONTENT OF THE PARENT FOLDER"
 ls -a ..
-echo "-------------------------------"
+echo "--------------------------------------------------------------"
 unzip ./InData.zip 
 if [ -f /tmp/.X1-lock ]; then
     rm -f /tmp/.X1-lock
@@ -13,5 +13,8 @@ if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
 vnc4server
+echo "--------------------------------------------------------------"
+echo $DISPLAY
+echo "--------------------------------------------------------------"
 ./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
-zip /OutData.zip ./OutData
+ls OutData
