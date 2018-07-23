85eadff42ca5c727bc9234c575c78cc1b5db3d99
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-23 10:25:47 +0200

    modifiche dopo II esecuzione su VLab

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-unzip /InData.zip
+unzip /InData.zip -d /SARWIND_LGMod
 if [ -f /tmp/.X1-lock ]; then
     rm -f /tmp/.X1-lock
 fi
@@ -6,5 +6,5 @@ if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
 vnc4server
-./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
-zip /OutData.zip ./OutData
+/SARWIND_LGMod/run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 /SARWIND_LGMod/InData/SARWIND_IO_Info.txt /SARWIND_LGMod/InData/SARWIND_Settings.txt
+zip /OutData.zip /SARWIND_LGMod/OutData
