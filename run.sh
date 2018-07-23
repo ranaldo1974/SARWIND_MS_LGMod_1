3fdef939ca11fcbf980187f2eb1fac4f21ad74db
ATom93 <37183327+ATom93@users.noreply.github.com>
2018-07-23 12:43:33 +0200

    cambiamenti generali ai files di VLab

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-echo ""
+echo "-------------------------------"
 echo "CONTENT OF THE FOLDER"
 ls -a .
-echo "\n"
-unzip ./InData.zip -d /SARWIND_LGMod
+echo "-------------------------------"
+echo "CONTENT OF THE PARENT FOLDER"
+ls -a ..
+echo "-------------------------------"
+unzip ./InData.zip 
 if [ -f /tmp/.X1-lock ]; then
     rm -f /tmp/.X1-lock
 fi
@@ -10,5 +13,5 @@ if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
 vnc4server
-/SARWIND_LGMod/run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 /SARWIND_LGMod/InData/SARWIND_IO_Info.txt /SARWIND_LGMod/InData/SARWIND_Settings.txt
-zip /OutData.zip /SARWIND_LGMod/OutData
+/run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 /InData/SARWIND_IO_Info.txt /InData/SARWIND_Settings.txt
+zip /OutData.zip /OutData
