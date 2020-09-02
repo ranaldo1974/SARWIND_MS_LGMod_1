0b267cc7e0c2ddfe4518130746f5e0fd7ddb4506
ranaldo1974 <42975645+ranaldo1974@users.noreply.github.com>
2020-09-02 12:15:09 +0200

    Update run.sh
    Modifica per versione Multi-Scale

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ if [ -d /tmp/.X11-unix ]; then
 fi
 vnc4server
 export DISPLAY=$HOSTNAME:1
-./run_SARWIND_LGMod_v401.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
+./run_SARWIND_MS_LGMod.sh /MCR_R2011b/v716 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
 zip -r OutData.zip OutData
 find ./OutData -name *_Results.txt -exec mv {} . \;
 find . -name *_Results.txt -exec mv {} SARWIND_LGMod_Results.txt \;
