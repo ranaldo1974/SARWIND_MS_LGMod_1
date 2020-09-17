3a928871710c7e79baf05e8a8800287c4102c42a
ranaldo1974 <42975645+ranaldo1974@users.noreply.github.com>
2020-09-17 13:49:48 +0200

    Update run.sh
    Following instructions:
    find ./OutData/*Multiscale* -name *_Results.txt -exec mv {} . \;
    find . -name *_Results.txt -exec mv {} SARWIND_LGMod_Results.txt \;
    replaced by:
    if [ -d ./OutData/*Multiscale* ] 
    then
        find ./OutData/*Multiscale* -name *_Results.txt -exec cp {} ./SARWIND_LGMod_Results.txt
    else
        find ./OutData -name *_Results.txt -exec cp {} ./SARWIND_LGMod_Results.txt
    fi

--- run.sh
+++ run.sh
@@ -2,17 +2,26 @@ echo "--------------------------------------------------------------"
 echo "CONTENT OF THE CURRENT FOLDER"
 ls -a .
 echo "--------------------------------------------------------------"
+
 unzip ./InData.zip 
+
 if [ -f /tmp/.X1-lock ]; then
     rm -f /tmp/.X1-lock
 fi
 if [ -d /tmp/.X11-unix ]; then
     rm -rf /tmp/.X11-unix
 fi
+
 vnc4server
+
 export DISPLAY=$HOSTNAME:1
 ./run_SARWIND_MS_LGMod.sh /MCR_R2016b/v91 ./InData/SARWIND_IO_Info.txt ./InData/SARWIND_Settings.txt
+
 zip -r OutData.zip OutData
-ls -l
-find ./OutData/*Multiscale* -name *_Results.txt -exec mv {} . \;
-find . -name *_Results.txt -exec mv {} SARWIND_LGMod_Results.txt \;
+
+if [ -d ./OutData/*Multiscale* ] 
+then
+    find ./OutData/*Multiscale* -name *_Results.txt -exec cp {} ./SARWIND_LGMod_Results.txt
+else
+    find ./OutData -name *_Results.txt -exec cp {} ./SARWIND_LGMod_Results.txt
+fi
